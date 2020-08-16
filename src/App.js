446d57a5aa4019cafe140659101f7a6c549bcceb
Matheus Cesar <matheuscla@yahoo.com.br>
2020-08-16 20:22:28 -0300

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { SideMenu, Title } from './styles/components';
+
 function App() {
   return (
-    <div>
-      Hi, there
-    </div>
+    <>
+      <SideMenu>
+        <Title>Online Editor</Title>
+      </SideMenu>
+    </>
   );
 }
 
---
+++ src/styles/components.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SideMenu = styled.div`
+  width: 256px;
+  border-right: 1px solid rgba(67, 70, 85, 0.4);
+  height: 100%;
+  padding: 40px 30px;
+`
+
+export const Title = styled.h1`
+  font-size: 24px;
+`
