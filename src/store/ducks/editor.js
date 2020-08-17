a408c3d86adaee3e9c86d343ea89ce6c21f40f73
Matheus Cesar <matheuscla@yahoo.com.br>
2020-08-17 19:03:24 -0300

    Add loading to editor ducks

--- src/store/ducks/editor.js
+++ src/store/ducks/editor.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const Types = {
   GET_TREE: 'GET_TREE',
-  GET_FILE: 'GET_FILE'
+  GET_FILE: 'GET_FILE',
+  SET_LOADING: 'SET_LOADING'
 }
 
 export const getTree = () => async dispatch => {
@@ -18,22 +19,29 @@ export const getTree = () => async dispatch => {
 
 export const getFile = id => async dispatch => {
   try {
+    dispatch({ type: Types.SET_LOADING, payload: true });
+
     const { data } = await axios.get(`https://my-json-server.typicode.com/open-veezoo/editor/files/${id}`);
 
     return dispatch({ type: Types.GET_FILE, payload: data });
   } catch(e) {
     console.error(e);
     throw e;
+  } finally {
+    dispatch({ type: Types.SET_LOADING, payload: false });
   }
 }
 
 const INITIAL_STATE = {
   tree: [],
-  selected_file: null
+  selected_file: null,
+  loading: false
 }
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
+    case Types.SET_LOADING:
+      return { ...state, loading: action.payload };
     case Types.GET_TREE:
       return { ...state, tree: action.payload };
     case Types.GET_FILE:
